d2651cdba38d66948f8cd20596f4a51eb4d64e0a
WanzBotz <100239769+Syazwan124@users.noreply.github.com>
2022-06-16 13:00:56 +0800

    Update infomursid.js

--- plugins/infomursid.js
+++ plugins/infomursid.js
@@ -1,21 +1,21 @@
 let handler  = async (m, { conn, usedPrefix }) => { 
 conn.reply(m.chat, `
 ╭─˗ˏˋ *INFORMASI OWNER* ´ˎ˗
-│ ✎ _Nama_ : Mursid S
-│ ✎ _Sekolah_ : SMKN 2 Wonosari
-│ ✎ _Umur_ : 17
-│ ✎ _Asal_ : Yogyakarta
+│ ✎ _Nama_ : Wanz
+│ ✎ _Sekolah_ : SMKS 
+│ ✎ _Umur_ : 16
+│ ✎ _Asal_ : Kedah
 │ ✎ _Status_ : Pelajar SMK
 │ ✎ _Official Grup 1_ :
-│    https://chat.whatsapp.com/LPFQ2X1cnihB0fb8F8cZau
+│    https://chat.whatsapp.com/JxD4ZjQiLVSCohWaRmyv0V
 │ ✎ _Official Grup 2_ :
-│    https://chat.whatsapp.com/HjRHck1G3WRHOx97fJkdMN
+│    https://chat.whatsapp.com/HbbYGD9VkoKBUQXG1vOLke
 │ ✎ _Official Grup 3_ :
-│    https://chat.whatsapp.com/HcGcIB09sIvKrGytO8yfFn
+│    https://chat.whatsapp.com/FJUrvlGsiFhI0jvv5EE3rC
 │ ✎ _Instagram_ : 
-│    instagram.com/mursid.st
+│    instagram.com/MidWanzofficial
 │ ✎ _WhatsApp_ :
-│    wa.me/6288233832771
+│    wa.me/60195471037
 ╰───────────────────
 `.trim(), m)
 }
